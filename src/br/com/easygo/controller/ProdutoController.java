de16993832ad279b53ebcdfab1eb58d372d12d03
LuizaSapucaia <45321536+LuizaSapucaia@users.noreply.github.com>
2018-11-27 21:40:13 -0200

    Add files via upload

--- src/br/com/easygo/controller/ProdutoController.java
+++ src/br/com/easygo/controller/ProdutoController.java
@@ -1,8 +1,9 @@
 package br.com.easygo.controller;
 
-import java.sql.SQLException;
-
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -10,7 +11,7 @@ import javax.ws.rs.core.Response;
 
 import com.google.gson.Gson;
 
-import br.com.easygo.dao.ConnectionFactory;
+import br.com.easygo.dao.ProdutoDao;
 import br.com.easygo.model.Produto;
 
 @Path("/produto")
@@ -20,14 +21,14 @@ public class ProdutoController {
 	@Produces("application/json")
 	public Response produtos() {
 		Gson gson = new Gson();
-		Produto produto = new Produto();
-		try {
-			ConnectionFactory.getInstance().getConnection();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return Response.status(200).entity(gson.toJson(produto)).build();
+		
+		ProdutoDao dao = new ProdutoDao();
+		
+		Produto[] produtos = dao.getProdutos();
+		
+		String retorno = gson.toJson(produtos, Produto[].class);
+		
+		return Response.status(200).entity(retorno).build();
 	}
 	
 	@GET
@@ -35,8 +36,49 @@ public class ProdutoController {
 	@Path("/{codigo}")
 	public Response produto(@PathParam("codigo") int codigo) {
 		Gson gson = new Gson();
-		Produto produto = new Produto();
-		produto.setId(codigo);
+		
+		ProdutoDao dao = new ProdutoDao();
+		
+		Produto produto = dao.getProdutoByCodigo(codigo);
+		
 		return Response.status(200).entity(gson.toJson(produto)).build();
+	}
+	
+	@POST
+	public Response insereProduto(String body){
+		int status;
+		
+		Gson gson = new Gson();
+		
+		ProdutoDao dao = new ProdutoDao();
+		
+		status = dao.salvaRegistro(gson.fromJson(body, Produto.class)) ? 200 : 400;
+		
+		return Response.status(status).build();
+	}
+	
+	@PUT
+	public Response atualizaProduto(String body){
+		int status;
+		
+		Gson gson = new Gson();
+		
+		ProdutoDao dao = new ProdutoDao();
+		
+		status = dao.salvaRegistro(gson.fromJson(body, Produto.class)) ? 200 : 400;
+		
+		return Response.status(status).build();
+	}
+	
+	@DELETE
+	@Path("/{codigo}")
+	public Response deletaProduto(@PathParam("codigo") int codigo) {
+		int status;
+		
+		ProdutoDao dao = new ProdutoDao();
+		
+		status = dao.deletaRegistro(codigo) ? 200 : 400;
+		
+		return Response.status(status).build();
 	}
 }
